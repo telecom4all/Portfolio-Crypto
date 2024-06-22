7025057a85d3da0dd3f28fede68dc4b09e502f00
angelz07 <fabrice@telecom4all.be>
2024-06-22 23:25:14 +0200

    d

--- portfolio_crypto_addon/run.sh
+++ portfolio_crypto_addon/run.sh
@@ -4,7 +4,7 @@
 mkdir -p /config/custom_components/portfolio_crypto
 
 # Copiez le composant personnalisé dans le répertoire custom_components de Home Assistant
-cp -r /app/portfolio_crypto/* /config/custom_components/portfolio_crypto/
+cp -r /app/custom_components/portfolio_crypto/* /config/custom_components/portfolio_crypto/
 
 # Démarrez l'application Flask avec Gunicorn
 exec gunicorn --bind 0.0.0.0:5000 portfolio_crypto.wsgi:app
