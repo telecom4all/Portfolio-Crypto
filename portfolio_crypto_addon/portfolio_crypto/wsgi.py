02d37a3fecd2725f7751d58dbf24fd4c36fdd1ca
angelz07 <fabrice@telecom4all.be>
2024-07-03 14:19:54 +0200

    https

--- portfolio_crypto_addon/portfolio_crypto/wsgi.py
+++ portfolio_crypto_addon/portfolio_crypto/wsgi.py
@@ -4,6 +4,10 @@ Ce fichier est le point d'entrée pour Gunicorn afin de lancer l'application Fla
 """
 
 from portfolio_crypto.portfolio_crypto import app
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+# Appliquer ProxyFix pour prendre en compte les en-têtes du reverse proxy
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
