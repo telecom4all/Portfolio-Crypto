7cc1cf1a8a2a83b6449c59e9cbb96580f995e37e
angelz07 <fabrice@telecom4all.be>
2024-07-05 18:10:44 +0200

    Update __init__.py

--- custom_components/portfolio_crypto/__init__.py
+++ custom_components/portfolio_crypto/__init__.py
@@ -1,6 +1,6 @@
 import logging
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, ServiceCall, ServiceResponse, SupportsResponse
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 from homeassistant.helpers import device_registry as dr, entity_registry as er
 from datetime import timedelta, datetime
@@ -8,10 +8,17 @@ import aiohttp
 import async_timeout
 import asyncio
 import os
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
 from .const import DOMAIN, COINGECKO_API_URL
 from .db import save_crypto, load_crypto_attributes, delete_crypto_db
 
 _LOGGER = logging.getLogger(__name__)
+
+# Définir le schéma de validation pour le service
+FETCH_CRYPTOS_SCHEMA = vol.Schema({
+    vol.Required('entry_id'): cv.string,
+})
 
 async def async_setup(hass: HomeAssistant, config: dict):
     """Configurer l'intégration via le fichier configuration.yaml (non utilisé ici)"""
@@ -38,6 +45,35 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         await initialize_database(entry, hass)
 
     await hass.config_entries.async_forward_entry_setup(entry, "sensor")
+
+    async def async_fetch_cryptos_service(call: ServiceCall) -> ServiceResponse:
+        entry_id = call.data.get("entry_id")
+        return_response = call.data.get("return_response", True)
+        _LOGGER.debug(f"Service fetch_cryptos called with entry_id: {entry_id}, return_response: {return_response}")
+        coordinator = hass.data[DOMAIN].get(entry_id)
+        if coordinator:
+            cryptos = await coordinator.load_cryptos_from_db(entry_id)
+            if cryptos is not None:
+                _LOGGER.debug(f"Cryptocurrencies retrieved: {cryptos}")
+                if return_response:
+                    return {"data": {"cryptos": cryptos}}
+                else:
+                    return {"data": {"status": "success", "cryptos": cryptos}}
+            else:
+                _LOGGER.error("Error retrieving cryptocurrencies")
+                raise ValueError("Error retrieving cryptocurrencies")
+        else:
+            _LOGGER.error(f"No coordinator found for entry_id: {entry_id}")
+            raise ValueError(f"No coordinator found for entry_id: {entry_id}")
+
+    hass.services.async_register(
+        DOMAIN,
+        "fetch_cryptos",
+        async_fetch_cryptos_service,
+        schema=FETCH_CRYPTOS_SCHEMA,
+        supports_response=SupportsResponse.ONLY,
+    )
+
 
     async def async_delete_crypto_service(call):
         crypto_id = call.data.get("crypto_id")
@@ -172,21 +208,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     hass.services.async_register(DOMAIN, "import_db", async_import_db_service)
 
-    async def async_fetch_cryptos_service(call):
-        entry_id = call.data.get("entry_id")
-        _LOGGER.debug(f"Service fetch_cryptos appelé avec entry_id: {entry_id}")
-        coordinator = hass.data[DOMAIN].get(entry_id)
-        if coordinator:
-            cryptos = await coordinator.load_cryptos_from_db(entry_id)
-            if cryptos is not None:
-                _LOGGER.debug(f"Cryptomonnaies récupérées: {cryptos}")
-                # Process or return the cryptos as needed
-            else:
-                _LOGGER.error("Erreur lors de la récupération des cryptomonnaies")
-        else:
-            _LOGGER.error(f"Aucun coordinator trouvé pour l'entry_id: {entry_id}")
-
-    hass.services.async_register(DOMAIN, "fetch_cryptos", async_fetch_cryptos_service)
+    
 
     async def async_fetch_transactions_service(call):
         entry_id = call.data.get("entry_id")
@@ -204,8 +226,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
             _LOGGER.error(f"Aucun coordinator trouvé pour l'entry_id: {entry_id}")
 
     hass.services.async_register(DOMAIN, "fetch_transactions", async_fetch_transactions_service)
-
-
 
     return True
 
@@ -392,7 +412,7 @@ class PortfolioCryptoCoordinator(DataUpdateCoordinator):
                 _LOGGER.error(f"Error fetching CoinGecko data: {e}")
                 return None
         return None
-
+    
 
 
     async def save_crypto_to_db(self, entry_id, crypto_name, crypto_id):
@@ -562,3 +582,149 @@ class PortfolioCryptoCoordinator(DataUpdateCoordinator):
         except Exception as e:
             _LOGGER.error(f"Exception lors de l'importation de la base de données: {e}")
             return False
+
+
+
+
+
+
+----------------------------------
+----------------------------------
+
+##################
+# config_flow.py # 
+##################
+
+import logging
+import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.core import callback
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from .const import DOMAIN, COINGECKO_API_URL
+
+_LOGGER = logging.getLogger(__name__)
+
+@config_entries.HANDLERS.register(DOMAIN)
+class PortfolioCryptoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title=user_input["name"], data={"name": user_input["name"], "cryptos": [], "initialized": False})
+
+        return self.async_show_form(
+            step_id="user", data_schema=vol.Schema({"name": str})
+        )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return PortfolioCryptoOptionsFlowHandler(config_entry)
+
+class PortfolioCryptoOptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        if user_input is not None:
+            return await self.async_step_add_crypto(user_input)
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema({
+                vol.Required("crypto_name_or_id", description="Nom ou ID de la cryptomonnaie"): str,
+            }),
+            errors={},
+        )
+
+    async def async_step_add_crypto(self, user_input=None):
+        if user_input is not None:
+            crypto_name_or_id = user_input.get("crypto_name_or_id")
+            session = async_get_clientsession(self.hass)
+            async with session.get(f"{COINGECKO_API_URL}?query={crypto_name_or_id}") as response:
+                if response.status == 200:
+                    data = await response.json()
+                    crypto_id = None
+                    crypto_name = None
+                    for coin in data:
+                        if coin['name'].lower() == crypto_name_or_id.lower() or coin['id'].lower() == crypto_name_or_id.lower():
+                            crypto_id = coin['id']
+                            crypto_name = coin['name']
+                            break
+                    if crypto_id:
+                        return self.async_show_form(
+                            step_id="confirm_add_crypto",
+                            data_schema=vol.Schema({
+                                vol.Required("crypto_name", default=crypto_name, description="Nom de la cryptomonnaie"): str,
+                                vol.Required("crypto_id", default=crypto_id, description="ID de la cryptomonnaie"): str,
+                            }),
+                            errors={},
+                        )
+                    else:
+                        return self.async_show_form(
+                            step_id="init",
+                            data_schema=vol.Schema({
+                                vol.Required("crypto_name_or_id", description="Nom ou ID de la cryptomonnaie"): str,
+                            }),
+                            errors={"base": "crypto_not_found"},
+                        )
+                else:
+                    return self.async_show_form(
+                        step_id="init",
+                        data_schema=vol.Schema({
+                            vol.Required("crypto_name_or_id", description="Nom ou ID de la cryptomonnaie"): str,
+                        }),
+                        errors={"base": "api_error"},
+                    )
+
+    async def async_step_confirm_add_crypto(self, user_input=None):
+        errors = {}
+        if user_input is not None:
+            crypto_name = user_input.get("crypto_name")
+            crypto_id = user_input.get("crypto_id")
+
+            #_LOGGER.error(f"crypto_name : {crypto_name}")
+            #_LOGGER.error(f"crypto_id : {crypto_id}")
+
+            # Vérifier si la crypto est déjà enregistrée dans l'intégration
+            cryptos = self.config_entry.options.get("cryptos", [])
+            valid_cryptos = [crypto for crypto in cryptos if isinstance(crypto, dict) and "id" in crypto]
+
+            if any(crypto["id"] == crypto_id for crypto in valid_cryptos):
+                errors["base"] = "crypto_already_exists"
+            else:
+                # Vérifier si la crypto existe déjà dans la base de données
+                session = async_get_clientsession(self.hass)
+                async with session.get(f"http://localhost:5000/load_cryptos/{self.config_entry.entry_id}") as response:
+                    if response.status == 200:
+                        db_cryptos = await response.json()
+                        if any(crypto[1] == crypto_id for crypto in db_cryptos):  # Assurez-vous que la structure des données correspond à votre base de données
+                            errors["base"] = "crypto_already_in_db"
+                        else:
+                            # Ajoute la nouvelle crypto
+                            coordinator = self.hass.data[DOMAIN].get(self.config_entry.entry_id)
+                            if coordinator:
+                                success = await coordinator.add_crypto(crypto_name, crypto_id)
+                                if success:
+                                    # Met à jour les options de l'entrée de configuration
+                                    valid_cryptos.append({"name": crypto_name, "id": crypto_id})
+                                    self.hass.config_entries.async_update_entry(self.config_entry, options={**self.config_entry.options, "cryptos": valid_cryptos})
+
+                                    # Reconfigure les entités pour ajouter les nouvelles cryptomonnaies
+                                    await self.hass.config_entries.async_forward_entry_unload(self.config_entry, "sensor")
+                                    await self.hass.config_entries.async_forward_entry_setup(self.config_entry, "sensor")
+
+                                    return self.async_create_entry(title=crypto_name, data={})
+                    else:
+                        errors["base"] = "db_error"
+
+        return self.async_show_form(
+            step_id="confirm_add_crypto",
+            data_schema=vol.Schema({
+                vol.Required("crypto_name", default=user_input.get("crypto_name"), description="Nom de la cryptomonnaie"): str,
+                vol.Required("crypto_id", default=user_input.get("crypto_id"), description="ID de la cryptomonnaie"): str,
+            }),
+            errors=errors,
+        )
+
+
