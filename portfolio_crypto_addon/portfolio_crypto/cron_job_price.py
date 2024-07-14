5a7705a5f18e59e7acad766a5b379e3e0d28a07a
angelz07 <fabrice@telecom4all.be>
2024-07-14 22:43:57 +0200

    dd

--- portfolio_crypto_addon/portfolio_crypto/cron_job_price.py
+++ portfolio_crypto_addon/portfolio_crypto/cron_job_price.py
@@ -1,12 +1,76 @@
 import logging
 import asyncio
 import aiocron
-from portfolio_crypto.price_updater import update_crypto_prices
+import sqlite3
+#import threading
+import requests
+from datetime import datetime
 
+PATH_DB_BASE = "/config/portfolio_crypto"
+UPDATE_INTERVAL_PRICE_UPDATER = 120
+COINGECKO_API_URL = "https://api.coingecko.com/api/v3/coins/list"
+COINGECKO_API_URL_PRICE = "https://api.coingecko.com/api/v3"
 
 # Configurer les logs
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(name)s:%(message)s')
 
+
+def get_crypto_list():
+    conn = sqlite3.connect(f'{PATH_DB_BASE}/list_crypto.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT crypto_id FROM cryptos')
+    cryptos = cursor.fetchall()
+    conn.close()
+    return [crypto[0] for crypto in cryptos]
+
+def update_crypto_price(crypto_id):
+    try:
+        url = f"{COINGECKO_API_URL}?ids={crypto_id}&vs_currencies=usd"
+        logging.info(f"URL PRICE UPDATER {url}")
+        response = requests.get(url)
+        if response.status_code == 200:
+            price = response.json().get(crypto_id, {}).get('usd', 0)
+            if price:
+                logging.info(f"Prix récupéré pour {crypto_id}: {price}")
+                save_crypto_price(crypto_id, price)
+            else:
+                logging.error(f"Prix non trouvé dans la réponse pour {crypto_id}: {response.json()}")
+        else:
+            logging.error(f"Échec de la récupération du prix pour {crypto_id}: {response.status_code}")
+    except Exception as e:
+        logging.error(f"Erreur lors de la mise à jour du prix pour {crypto_id}: {e}")
+
+
+def save_crypto_price(crypto_id, price):
+    conn = sqlite3.connect(f'{PATH_DB_BASE}/cache_prix_crypto.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS prices (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            crypto_id TEXT,
+            price REAL,
+            timestamp TEXT
+        )
+    ''')
+    cursor.execute('''
+        INSERT INTO prices (crypto_id, price, timestamp)
+        VALUES (?, ?, ?)
+    ''', (crypto_id, price, datetime.now().isoformat()))
+    conn.commit()
+    conn.close()
+
+
+
+async def update_crypto_prices():
+    logging.info("Starting to update crypto prices...")
+    cryptos = await get_crypto_list()
+    for crypto_id in cryptos:
+        logging.info(f"Updating price for {crypto_id}")
+        await update_crypto_price(crypto_id)
+        await asyncio.sleep(60)  # Sleep for 1 minute
+    logging.info("Finished updating crypto prices")
+
+    
 @aiocron.crontab('*/5 * * * *')
 async def scheduled_task():
     logging.info("Tâche cron démarrée")
@@ -22,7 +86,7 @@ async def main():
     await scheduled_task()
     # Keep the script running to allow the cron job to run
     while True:
-        await asyncio.sleep(120)  # Sleep for 1 minute
+        await asyncio.sleep(UPDATE_INTERVAL_PRICE_UPDATER)  # Sleep for 1 minute
 
 if __name__ == "__main__":
     asyncio.run(main())
--- portfolio_crypto_addon/portfolio_crypto/price_updater.py
+++ portfolio_crypto_addon/portfolio_crypto/price_updater.py
@@ -28,50 +28,6 @@ logging.basicConfig(level=logging.INFO)
 # Ajouter le gestionnaire au logger
 #logger.addHandler(file_handler)
 
-def get_crypto_list():
-    conn = sqlite3.connect(f'{PATH_DB_BASE}/list_crypto.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT crypto_id FROM cryptos')
-    cryptos = cursor.fetchall()
-    conn.close()
-    return [crypto[0] for crypto in cryptos]
-
-def update_crypto_price(crypto_id):
-    try:
-        url = f"{COINGECKO_API_URL}?ids={crypto_id}&vs_currencies=usd"
-        logging.info(f"URL PRICE UPDATER {url}")
-        response = requests.get(url)
-        if response.status_code == 200:
-            price = response.json().get(crypto_id, {}).get('usd', 0)
-            if price:
-                logging.info(f"Prix récupéré pour {crypto_id}: {price}")
-                save_crypto_price(crypto_id, price)
-            else:
-                logging.error(f"Prix non trouvé dans la réponse pour {crypto_id}: {response.json()}")
-        else:
-            logging.error(f"Échec de la récupération du prix pour {crypto_id}: {response.status_code}")
-    except Exception as e:
-        logging.error(f"Erreur lors de la mise à jour du prix pour {crypto_id}: {e}")
-
-
-def save_crypto_price(crypto_id, price):
-    conn = sqlite3.connect(f'{PATH_DB_BASE}/cache_prix_crypto.db')
-    cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS prices (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            crypto_id TEXT,
-            price REAL,
-            timestamp TEXT
-        )
-    ''')
-    cursor.execute('''
-        INSERT INTO prices (crypto_id, price, timestamp)
-        VALUES (?, ?, ?)
-    ''', (crypto_id, price, datetime.now().isoformat()))
-    conn.commit()
-    conn.close()
-
 
 
 async def add_crypto_to_general_db(crypto_name, crypto_id):
@@ -92,13 +48,3 @@ async def add_crypto_to_general_db(crypto_name, crypto_id):
     conn.commit()
     conn.close()
 
-
-
-async def update_crypto_prices():
-    logging.info("Starting to update crypto prices...")
-    cryptos = await get_crypto_list()
-    for crypto_id in cryptos:
-        logging.info(f"Updating price for {crypto_id}")
-        await update_crypto_price(crypto_id)
-        await asyncio.sleep(60)  # Sleep for 1 minute
-    logging.info("Finished updating crypto prices")
